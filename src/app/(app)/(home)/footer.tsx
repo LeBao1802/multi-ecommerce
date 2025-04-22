c1070fc01d94d56436fd2b566cf3db95207b70f6
bao1802 <kelvinle1802@gmail.com>
2025-04-22 15:25:39 +0700

    06: categories finalization

--- src/app/(app)/(home)/footer.tsx
+++ src/app/(app)/(home)/footer.tsx
@@ -2,7 +2,12 @@ export const Footer = () => {
   return (
     <footer className="flex border-t justify-between font-medium p-6">
       <div className="flex items-center gap-2">
-        <p>Gameloft.CO</p>
+        <p>Công Ty TNHH Công Nghệ Asus (Việt Nam)</p>
+        <p>
+          Địa chỉ: 285 Cách Mạng Tháng Tám, Phường 12, Quận 10, Thành phố Hồ Chí
+          Minh, Việt Nam
+        </p>
+        <p>Điện thoại: 1800 65 88</p>
       </div>
     </footer>
   );
